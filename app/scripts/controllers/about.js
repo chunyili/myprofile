04a72a33b47536f77865bb4863e5bb5af604a45a
jilongs <sjilongarchitect@gmail.com>
2016-03-01 23:44:15 -0800

    Init the project with Yoeman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myprofileApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the myprofileApp
+ */
+angular.module('myprofileApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
