cca2a3c29b0a3ef2eebc137059db87235f0322ec
jilongs <sjilongarchitect@gmail.com>
2016-03-02 00:13:53 -0800

    support npm start

---
+++ server.js
@@ -0,0 +1,10 @@
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+app.use(express.static("dist"));
+
+app.listen(3000, function() {
+  console.log('listening');
+});
