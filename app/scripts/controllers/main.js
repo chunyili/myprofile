c0882619a53912d2e299a9a8782f62181177ea24
jilongs <sjilongarchitect@gmail.com>
2016-03-08 01:15:14 -0800

    including fullpage

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,11 @@
  */
 angular.module('myprofileApp')
   .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    //destroying
+    if($('html').hasClass('fp-enabled')){
+      $.fn.fullpage.destroy('all');
+    };
+    $('#fullpage').fullpage({
+      sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000']
+    });
   });
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -31,6 +31,9 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/fullpage.js/jquery.fullPage.js',
+      'bower_components/fullpage.js/vendors/jquery.easings.min.js',
+      'bower_components/fullpage.js/vendors/jquery.slimscroll.min.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
